2e0fc71fbb607a29eb30d631403a787bbe74cbce
M0N1SH <95116040+M0N1SHGUPTA@users.noreply.github.com>
2026-08-17 15:59:35 +0530

    Add additional CORS origins for middleware

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ app = FastAPI()
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://ziplink-70cf.onrender.com",
+    "http://ziplink-70cf.onrender.com",
 ]
 
 app.add_middleware(
